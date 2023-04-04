3c510fa3d79d6b0649dcafeb5e56267773cc62b3
Victor Queiroz <victorcqueirozg@gmail.com>
2023-04-04 13:02:02 -0300

    fix RingBuffer issue

--- opus/RingBuffer.ts
+++ opus/RingBuffer.ts
@@ -1,38 +1,50 @@
+declare const console: {
+    log: (...args: any[]) => void;
+};
+
 export default class RingBuffer {
     #arrayBuffer;
     #readOffset;
     #writeOffset;
     #frameSize;
-    public constructor(frameSize: number){
+    public constructor(frameSize: number) {
         this.#readOffset = 0;
         this.#writeOffset = 0;
-        this.#arrayBuffer = new ArrayBuffer(1024*1024*4);
+        this.#arrayBuffer = new ArrayBuffer(1024 * 1024 * 4);
         this.#frameSize = frameSize;
     }
-    #view(){
+    #view() {
         return new Float32Array(this.#arrayBuffer);
     }
-    public write(value: Float32Array){
+    public write(value: Float32Array) {
         this.#maybeReallocate(value.length);
-        this.#view().set(value,this.#writeOffset);
+        this.#view().set(value, this.#writeOffset);
         this.#writeOffset += value.length;
     }
     public read(): Float32Array | null {
-        if(this.#writeOffset >= this.#frameSize){
-            const view = this.#view().subarray(this.#readOffset,this.#readOffset + this.#frameSize);
+        const remainingBytes = this.#writeOffset - this.#readOffset;
+        console.log(remainingBytes, this.#frameSize);
+        if (remainingBytes >= this.#frameSize) {
+            const view = this.#view().subarray(
+                this.#readOffset,
+                this.#readOffset + this.#frameSize
+            );
             this.#readOffset += this.#frameSize;
-            if(this.#readOffset >= this.#writeOffset){
+            if (this.#readOffset >= this.#writeOffset) {
                 this.#writeOffset = 0;
+                this.#readOffset = 0;
             }
             return view;
         }
         return null;
     }
-    #maybeReallocate(samples: number){
+    #maybeReallocate(samples: number) {
         const sampleCountInBytes = samples * Float32Array.BYTES_PER_ELEMENT;
-        if(this.#view().length <= samples){
+        if (this.#view().length <= samples) {
             const oldArrayBuffer = this.#arrayBuffer;
-            this.#arrayBuffer = new ArrayBuffer(oldArrayBuffer.byteLength + sampleCountInBytes + 1024*1024*4);
+            this.#arrayBuffer = new ArrayBuffer(
+                oldArrayBuffer.byteLength + sampleCountInBytes + 1024 * 1024 * 4
+            );
             this.#view().set(new Uint8Array(oldArrayBuffer));
         }
     }
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,8 @@
-import native from "../native";
-import Runtime from "../runtime/Runtime";
-import assert from "assert";
-import child_process from "child_process";
-import { Encoder, Decoder, RingBuffer } from "../opus";
+import native from '../native';
+import Runtime from '../runtime/Runtime';
+import assert from 'assert';
+import child_process from 'child_process';
+import { Encoder, Decoder, RingBuffer } from '../opus';
 
 async function createRuntime() {
     return new Runtime(await native());
@@ -27,27 +27,27 @@ async function testEncoderOpusEncoding() {
     const frameSizeInBytes =
         frameSizeInSamples * Float32Array.BYTES_PER_ELEMENT;
     const enc = new Encoder(runtime, 48000, 1, 2048, 10000, frameSizeInBytes);
-    const pcm = child_process.spawn("arecord", [
-        "-r",
-        "48000",
-        "-f",
-        "FLOAT_LE",
-        "-d",
-        "4",
+    const pcm = child_process.spawn('arecord', [
+        '-r',
+        '48000',
+        '-f',
+        'FLOAT_LE',
+        '-d',
+        '4',
         `--buffer-size=${frameSizeInBytes}`,
     ]);
     const dec = new Decoder(runtime, 48000, 1, frameSizeInSamples);
-    const aplay = child_process.spawn("aplay", [
-        "-f",
-        "FLOAT_LE",
-        "-r",
-        "48000",
-        "-c",
-        "1",
-        "-i",
+    const aplay = child_process.spawn('aplay', [
+        '-f',
+        'FLOAT_LE',
+        '-r',
+        '48000',
+        '-c',
+        '1',
+        '-i',
     ]);
     const ringBuffer = new RingBuffer(frameSizeInSamples);
-    pcm.stdout.on("data", (chunk) => {
+    pcm.stdout.on('data', (chunk) => {
         assert.strict.ok(Buffer.isBuffer(chunk));
         const buffer = new Float32Array(chunk.buffer);
         ringBuffer.write(buffer);
@@ -72,7 +72,7 @@ async function testEncoderOpusEncoding() {
             )
         );
     });
-    pcm.stdout.on("end", () => {
+    pcm.stdout.on('end', () => {
         aplay.stdin.end();
     });
 }
@@ -83,6 +83,13 @@ async function testRingBuffer() {
     assert.strict.equal(ringBuffer.read(), null);
     ringBuffer.write(new Float32Array(880));
     assert.strict.deepEqual(ringBuffer.read(), new Float32Array(2880));
+    assert.strict.equal(ringBuffer.read(), null);
+    ringBuffer.write(new Float32Array(2000).fill(0.75));
+    ringBuffer.write(new Float32Array(880).fill(0.75));
+    assert.strict.deepEqual(
+        ringBuffer.read(),
+        new Float32Array(2880).fill(0.75)
+    );
 }
 
 (async () => {
