f46b29706aeb220bbafe16495f4ad8e39ebcd8e1
Victor Queiroz <victorcqueirozg@gmail.com>
2023-04-08 21:35:40 -0300

    remove Pointer export

--- runtime/index.ts
+++ runtime/index.ts
@@ -1,5 +1,4 @@
 export { default as Integer } from './Integer';
-export { default as Pointer } from './Pointer';
 export { default as Runtime } from './Runtime';
 export { default as Buffer } from './Buffer';
 export { default as ResourcesHolder, IResource } from './ResourcesHolder';
